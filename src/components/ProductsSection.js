5103a0637bb99c15bed20e7347ee5d1bdefe27a4
Mohd Muntajibuddin <mohdmuntajib5@gmail.com>
2025-09-21 20:47:27 +0530

    Make product logos clickable and remove product buttons

--- src/components/ProductsSection.js
+++ src/components/ProductsSection.js
@@ -1,11 +1,11 @@
 // src/components/ProductsSection.js
-import React from 'react';
-import { Box, Typography, Grid, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Box, Typography, Grid } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const ProductsSection = () => {
   return (
-    <Box sx={{ p: 4, textAlign: 'center' }}>
+    <Box sx={{ p: 4, textAlign: "center" }}>
       <Typography variant="h3" gutterBottom>
         Our Products
       </Typography>
@@ -13,31 +13,42 @@ const ProductsSection = () => {
       <Grid container spacing={4} justifyContent="center">
         {/* Product 1 */}
         <Grid item>
-          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Box
-              component="img"
-              src="/images/product1-logo.jpg"
-              alt="Product 1 Logo"
-              sx={{ height: 80, mb: 1 }}
-            />
-            <Button variant="contained" component={Link} to="/products/product1">
-              Product 1
-            </Button>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            {/* clickable logo that navigates to the product page */}
+            <Link to="/products/product1" aria-label="Go to Product 1 page" style={{ display: "inline-block" }}>
+              <Box
+                component="img"
+                src="/images/product1-logo.jpg"
+                alt="Product 1 Logo"
+                sx={{ height: 100, mb: 1, cursor: "pointer", borderRadius: 1 }}
+              />
+            </Link>
           </Box>
         </Grid>
 
         {/* Product 2 */}
         <Grid item>
-          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Box
-              component="img"
-              src="/images/product2-logo.jpg"
-              alt="Product 2 Logo"
-              sx={{ height: 80, mb: 1 }}
-            />
-            <Button variant="contained" component={Link} to="/products/product2">
-              Product 2
-            </Button>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Link to="/products/product2" aria-label="Go to Product 2 page" style={{ display: "inline-block" }}>
+              <Box
+                component="img"
+                src="/images/product2-logo.jpg"
+                alt="Product 2 Logo"
+                sx={{ height: 100, mb: 1, cursor: "pointer", borderRadius: 1 }}
+              />
+            </Link>
           </Box>
         </Grid>
       </Grid>
