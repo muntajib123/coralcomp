98205c43cb5e57f3244be6ec5142f091ed0b549e
Mohd Muntajibuddin <mohdmuntajib5@gmail.com>
2025-09-21 20:44:00 +0530

    Update Our Products section with new product logos and names

--- src/components/ProductsSection.js
+++ src/components/ProductsSection.js
@@ -11,30 +11,32 @@ const ProductsSection = () => {
       </Typography>
 
       <Grid container spacing={4} justifyContent="center">
+        {/* Product 1 */}
         <Grid item>
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Box
               component="img"
-              src="/images/met360-logo.png"
-              alt="Met 360 Logo"
-              sx={{ height: 60, mb: 1 }}
+              src="/images/product1-logo.jpg"
+              alt="Product 1 Logo"
+              sx={{ height: 80, mb: 1 }}
             />
-            <Button variant="contained" component={Link} to="/products/met360">
-              Met 360
+            <Button variant="contained" component={Link} to="/products/product1">
+              Product 1
             </Button>
           </Box>
         </Grid>
 
+        {/* Product 2 */}
         <Grid item>
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Box
               component="img"
-              src="/images/flarex-logo.png"
-              alt="Flarex Logo"
-              sx={{ height: 60, mb: 1 }}
+              src="/images/product2-logo.jpg"
+              alt="Product 2 Logo"
+              sx={{ height: 80, mb: 1 }}
             />
-            <Button variant="contained" component={Link} to="/products/flarex">
-              Flarex
+            <Button variant="contained" component={Link} to="/products/product2">
+              Product 2
             </Button>
           </Box>
         </Grid>
