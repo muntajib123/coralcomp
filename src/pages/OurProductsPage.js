3831ebc475868019bfffdc6141df125437c0ab7d
Mohd Muntajibuddin <mohdmuntajib5@gmail.com>
2025-09-22 02:30:36 +0530

    Remove old ProductsLanding and fix OurProductsPage

--- src/pages/OurProductsPage.js
+++
@@ -1,9 +0,0 @@
-// src/pages/OurProductsPage.js
-import React from "react";
-import ProductsLanding from "../components/ProductsLanding";
-
-const OurProductsPage = () => {
-  return <ProductsLanding />;
-};
-
-export default OurProductsPage;
